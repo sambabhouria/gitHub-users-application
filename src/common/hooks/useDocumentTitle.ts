33bc27b15c6df7b653c82d5a4904fa305b32cd41
Samba Diallo <samba.diallo@id-mac007.idtgv.cnit>
2021-05-01 13:19:25 +0200

    MacBook-Pro:users-app

--- src/common/hooks/useDocumentTitle.ts
+++
@@ -1,7 +0,0 @@
-import { useLayoutEffect } from 'react'
-
-export default function useDocumentTitle(title?: string): void {
-    useLayoutEffect(() => {
-        document.title = title ? title + ' | GitHub Users' : 'GitHub Users'
-    }, [title])
-}
