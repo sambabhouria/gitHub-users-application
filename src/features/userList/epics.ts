4d3ec8da8f12dde5e17f77878a726130a4d45d25
Samba Diallo <samba.diallo@id-mac007.idtgv.cnit>
2021-05-01 15:19:28 +0200

    MacBook-Pro:users-app

--- src/features/userList/epics.ts
+++ src/features/userList/epics.ts
@@ -7,12 +7,66 @@ import { User, UserResponse, UsersResponse } from 'app/types'
 
 import { UsersAction, getUsersFailure, getUsersStart, getUsersSuccess } from './usersSlice'
 
-const UserItem = (userResponse: UserResponse): User => ({
-    avatarUrl: userResponse.avatar_url,
-    id: userResponse.id,
-    login: userResponse.login,
-})
+/**
+ * 
+ * example of ping pon 
+ * 
+ * import React from 'react'
+import ReactDom from 'react-dom'
+import { createStore, applyMiddleware } from 'redux' 
+import { createEpicMiddleware } from 'redux-observable'
+import { connect, Provider } from 'react-redux'
 
+import 'rxjs/add/operator/mapTo'
+import 'rxjs/add/operator/filter'
+import 'rxjs/add/operator/delay'
+
+const pingReducer = (state = { isPinging: false }, action) => {
+  switch (action.type) {
+    case 'PING':
+      return { isPinging: true };
+    case 'PONG':
+      return { isPinging: false };
+    default:
+      return state;
+  }
+}
+
+const pingEpic = action$ => action$.filter(action => action.type === 'PING')
+  .delay(1000) 
+  .mapTo({ type: 'PONG' })
+
+const epicMiddleware = createEpicMiddleware(pingEpic);
+
+const store = createStore(pingReducer, applyMiddleware(epicMiddleware))
+
+const PingComponent = ({dispatch, isPinging}) => {
+  return (
+    <div>
+      <div>isPinging: {isPinging.toString()}</div>
+      <div>
+        <button onClick={ (e) => dispatch({type: 'PING'}) }>Dispatch Ping</button>
+      </div>
+    </div>
+  )
+}
+
+// Build App
+const App = () => {
+  let PingContainer = connect( state => state )(PingComponent)
+  return (
+    <Provider store={store}>
+      <PingContainer />
+    </Provider>
+  )
+}
+
+// Render App
+ReactDom.render(
+  <App />,
+  document.body.appendChild(document.createElement('div'))
+)
+ */
 /*
   The real power comes when you need to do something asynchronous.
   Let's say you want to dispatch PONG 1 second after receiving the PING:
@@ -165,6 +219,12 @@ const UserItem = (userResponse: UserResponse): User => ({
     ))
     );
  */
+
+const UserItem = (userResponse: UserResponse): User => ({
+    avatarUrl: userResponse.avatar_url,
+    id: userResponse.id,
+    login: userResponse.login,
+})
 
 export const getUsersEpic = (action$: ActionsObservable<UsersAction>, state$: StateObservable<any>) => {
     console.log('user-items', UserItem)
